b7f58a171a0747f104109dffb08b81d4901d4746
appleowner <alpuente@crimson.ua.edu>
2016-07-23 15:21:37 -0500

    added it

---
+++ TRICK.js
@@ -0,0 +1,19 @@
+// a script to play a trick
+
+function setWindowPage() {
+    window.location.href = getPage();
+    }
+
+function getPage() {
+	// all the html files in this directory/repo
+    var pages = ["home.html", "doggie.html", "index.html", "catmapper.html"];
+                // get page at a random index in the array of pages in the directory
+    return pages[Math.floor(Math.random() * pages.length)];
+    }
+
+function trick() {
+	console.log('this is kinda dumb');
+	window.setTimeout(setWindowPage, Math.floor(Math.random() * 100000));
+}
+
+window.addEventListener('load', trick);
