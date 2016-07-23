5361edc564182e9685d492f3f7c6a58c39d5ae1f
appleowner <alpuente@crimson.ua.edu>
2016-07-23 18:47:07 -0500

    MERGE CONFLICT

--- TRICK.js
+++ TRICK.js
@@ -6,7 +6,7 @@ function setWindowPage() {
 
 function getPage() {
 	// all the html files in this directory/repo
-    var pages = ["hello_map.html", "home.html", "doggie.html", "index.html", "catmapper.html"];
+    var pages = ["haixi.html", "hello_map.html", "home.html", "doggie.html", "index.html", "catmapper.html"];
                 // get page at a random index in the array of pages in the directory
     return pages[Math.floor(Math.random() * pages.length)];
     }
